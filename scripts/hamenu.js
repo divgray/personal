ff8531b856df42a232a488918e8e1b9d26080f03
divgray <dgiray@byupathway.edu>
2025-07-02 09:35:13 +0800

    updated index

--- scripts/hamenu.js
+++ scripts/hamenu.js
@@ -1,7 +1,15 @@
-const hamButton = document.querySelector('.menu');
-const navigation = document.querySelector('.nav');
+// const hamButton = document.querySelector('.menu');
+// const navigation = document.querySelector('.nav');
 
-hamButton.addEventListener('click', () => {
-    hamButton.classList.toggle('open');
-    navigation.classList.toggle('open');
+// hamButton.addEventListener('click', () => {
+//     hamButton.classList.toggle('open');
+//     navigation.classList.toggle('open');
+// });
+
+const btn = document.querySelector('.menu');
+const nav = document.querySelector('nav');
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open');
+    nav.classList.toggle('open');
 });
